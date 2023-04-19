1981ade03ac4e13893ae3eb79d407d995a80e19c
Cristian Murua <cristianmurua1995@gmail.com>
2023-04-18 21:37:59 -0300

    paginator fix

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import ReactPaginate from 'react-paginate';
 import Card from "./Card";
 import '../styles/List.css'
@@ -8,8 +8,19 @@ function List({tasks, deleteTask, updateTask}) {
     const [priorityFilter, setPriorityFilter] = useState('Todos');
     const [currentPage, setCurrentPage] = useState(0);
     const [tasksPerPage, setTasksPerPage] = useState(4);
+    const [searchValue, setSearchValue] = useState('');
 
-    const filteredTasks = tasks.filter(t => {
+
+
+    //Filtros y barra de busqueda
+
+    const searchTasks = (tasks, searchValue) => {
+      return tasks.filter(task => {
+        return task.title.toLowerCase().includes(searchValue.toLowerCase());
+      });
+    };
+
+    const filteredTasks = searchTasks(tasks, searchValue).filter(t => {
         if (stateFilter === 'Todos' && priorityFilter === 'Todos') {
           return true;
         } else if (stateFilter === 'Todos' && priorityFilter !== 'Todos') {
@@ -19,7 +30,7 @@ function List({tasks, deleteTask, updateTask}) {
         } else {
           return t.state === stateFilter && t.priority === priorityFilter;
         }
-      });
+      });     
       
 
       const handleStateFilterChange = e => {
@@ -30,16 +41,26 @@ function List({tasks, deleteTask, updateTask}) {
         setPriorityFilter(e.target.value);
       };
 
+    //Paginado
+
       function handlePageClick(data) {
         setCurrentPage(data.selected);
+        console.log(data)
       };
+
+
+      useEffect(() => {
+        if (filteredTasks.length <= tasksPerPage) {
+          setCurrentPage(0);
+        }
+      }, [filteredTasks, tasksPerPage]);
 
       const pageCount = Math.ceil(filteredTasks.length / tasksPerPage);
 
     return (
         <>
         <div className="filters">
-            <h3>Filtrar por</h3>
+          <div>
             <label>Estado: </label>
             <select data-testid='state' id="state-filter" value={stateFilter} onChange={handleStateFilterChange}>
               <option value="Todos">Todos</option>
@@ -47,7 +68,8 @@ function List({tasks, deleteTask, updateTask}) {
               <option value="En Proceso">En Proceso</option>
               <option value="Finalizada">Finalizada</option>
             </select>
-
+          </div>
+          <div>
             <label>Prioridad: </label>
             <select data-testid='priority' id="priority-filter" value={priorityFilter} onChange={handlePriorityFilterChange}>
               <option value="Todos">Todos</option>
@@ -55,6 +77,8 @@ function List({tasks, deleteTask, updateTask}) {
               <option value="Media">Media</option>
               <option value="Alta">Alta</option>
             </select>
+          </div>
+            <input type="text" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} placeholder="Buscar por título" />
         </div>
         <div >
             <ReactPaginate
@@ -66,11 +90,11 @@ function List({tasks, deleteTask, updateTask}) {
       </div>
 
         <div className="list">
-        {filteredTasks.slice(currentPage * tasksPerPage, (currentPage + 1) * tasksPerPage).map((t) => (
+        {filteredTasks.length !== 0 ? filteredTasks.slice(currentPage * tasksPerPage, (currentPage + 1) * tasksPerPage).map((t) => (
             <div data-testid='card' key={t.id}>
                 <Card key={t.id} task={t} deleteTask={deleteTask} updateTask={updateTask}/>
             </div>
-        ))}
+        )) : <h3>No hay Tareas</h3>}
         </div>
         </>
     )
